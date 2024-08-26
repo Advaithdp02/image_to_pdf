167e455650840e17935067e65847e86560928f95
Advaithdinesan02 <advaithdp@email.com>
2024-08-26 11:55:16 +0530

    creating images folder if it doesnt exist

--- script.py
+++ script.py
@@ -8,12 +8,13 @@ openimage=[]
 print('ENTER THE NAME FOR THE PDF ')
 name=input()
 
-
+if not os.path.exists(img_file):
+    os.mkdir("images")
 for i in img_item:
     openimage.append(Image.open(img_file+i))
 
 for i in openimage:
-    img_conv.append(i.convert('RGB'))
+     img_conv.append(i.convert('RGB'))
 
 
 img_conv[0].save(os.getcwd()+'/{}.pdf'.format(name),save_all=True,append_images=img_conv[1:])
